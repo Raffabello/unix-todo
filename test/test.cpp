6da410e7c8a08c19f785684deaa304a5f3b63425
Raffabello <that.raffaele95bin@gmail.com>
2024-10-26 06:54:17 +0200

    Added a simple test for interacting with the command line

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
-int main(void){
+int main(int arg, char** argv){
 
-    cout << "Hello from the cpp file" << endl;
+    if(strcmp(argv[1], "-a") == 0 || strcmp(argv[1], "--add") == 0){
+        if(strcmp(argv[2], "") == 0){
+            cout << "The task cannot be empty" << endl;
+        }
+        else {
+            cout << "TODO:" << argv[2] << endl;
+        }
+    }
+    else{
+        cout << "This command doesn\'t exist." << endl;
+    }
+
     return 0;
 }
