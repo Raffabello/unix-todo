7b0b811cff9a286ab269135481f29d951910c317
Raffabello <that.raffaele95bin@gmail.com>
2024-10-29 15:38:48 +0100

    Added the add feature to the program to add tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
+#include "../include/TaskManager.h"
 
+using namespace std;
 int main(int argc, char **argv){
-    
+    string command = argv[1];
+    if(command == "-a" || command == "-add"){
+        cout << "User asked to add a task";
+    }
+    else{
+        cout << "This command is not recognized" << endl;
+    }
     return 0;
 }
