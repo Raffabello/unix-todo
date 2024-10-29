a179d26628de4b80810f78e4c79f9d7701ce17c3
Raffabello <that.raffaele95bin@gmail.com>
2024-10-29 16:48:05 +0100

    Added the add function

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,19 @@
 
 using namespace std;
 int main(int argc, char **argv){
-    string command = argv[1];
-    if(command == "-a" || command == "-add"){
-        cout << "User asked to add a task";
+    if(argc == 3){
+        string command = argv[1];
+        if(command == "-a" || command == "--add"){
+            string task = argv[2];
+        }
+        else{
+            cout << "The command is not recognized" << endl;
+            return 1;
+        }
     }
     else{
-        cout << "This command is not recognized" << endl;
+        cerr << "The command is malformed" << endl;
+        return 1;
     }
     return 0;
 }
